189a06d662355b5fd6e91cbaa20606f11823a35b
Sena Isaka <sena_south_stars_1996_h8313@live.jp>
2023-12-31 23:34:50 +0900

    hammerspoonのFirefoxをArcに変更

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,9 +1,9 @@
 hs.hotkey.bind({"ctrl", "command"}, "1", function()
-  local finder = hs.application.find('Finder')
-  if finder:isFrontmost() then
-    finder:hide()
+  local arc = hs.application.find('Arc')
+  if arc:isFrontmost() then
+    hs.eventtap.keyStroke("cmd", "h")
   else
-    hs.application.launchOrFocus("/System/Library/CoreServices/Finder.app")
+    hs.application.launchOrFocus("/Applications/Arc.app")
   end
 end)
 
@@ -17,15 +17,6 @@ hs.hotkey.bind({"ctrl", "command"}, "2", function()
 end)
 
 hs.hotkey.bind({"ctrl", "command"}, "3", function()
-  local firefox = hs.application.find('firefox')
-  if firefox:isFrontmost() then
-    firefox:hide()
-  else
-    hs.application.launchOrFocus("/Applications/Firefox.app")
-  end
-end)
-
-hs.hotkey.bind({"ctrl", "command"}, "4", function()
   local slack = hs.application.find('slack')
   if slack:isFrontmost() then
     slack:hide()
@@ -34,11 +25,20 @@ hs.hotkey.bind({"ctrl", "command"}, "4", function()
   end
 end)
 
-hs.hotkey.bind({"ctrl", "command"}, "5", function()
+hs.hotkey.bind({"ctrl", "command"}, "4", function()
   local spark = hs.application.find('Spark Desktop')
   if spark:isFrontmost() then
     spark:hide()
   else
     hs.application.launchOrFocus("/Applications/Spark Desktop.app")
+  end
+end)
+
+hs.hotkey.bind({"ctrl", "command"}, "5", function()
+  local finder = hs.application.find('Finder')
+  if finder:isFrontmost() then
+    finder:hide()
+  else
+    hs.application.launchOrFocus("/System/Library/CoreServices/Finder.app")
   end
 end)
