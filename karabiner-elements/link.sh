864f6950193fca963b09b3565c548b76d52b5b4b
Sena Isaka <isase@SenanoMacBook-Pro.local>
2023-12-31 20:58:44 +0900

    karabinerのlink方法修正

--- karabiner-elements/link.sh
+++ karabiner-elements/link.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-
-ln -sf $PWD/karabiner-elements/complex_modifications.json ~/.config/karabiner/assets/complex_modifications/custom.json
-
-# $ ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+# シンボリックリンクではKarabinerが読み込めないのでファイルコピーする
+cp ~/.dotfiles/karabiner-elements/complex_modifications.json ~/.config/karabiner/assets/complex_modifications/custom.json
